57a708aed2b88c89da1d80c7b678280d1763ea0c
Sanket <sanket226@gmail.com>
2017-09-28 21:33:48 +0800

    Wire up TodoApp component

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,8 @@ module.exports = {
   resolve: {
     root: __dirname,
     alias: {
-      applicationStyles: 'app/styles/app.scss'
+      applicationStyles: 'app/styles/app.scss',
+      TodoApp: 'app/components/TodoApp.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
